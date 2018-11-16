62308711cc5c4c3635a6d5c450716c55a2e30845
Thomas Leuxner <tlx@leuxner.net>
2018-11-16 14:22:03 +0100

    Fixed comments

--- bin/doveldappw.sh
+++ bin/doveldappw.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 # Update Dovecot user password in LDAP
+# https://github.com/tleuxner/ldap-virtualMail
 # Thomas Leuxner <tlx@leuxner.net> 01-11-2018
 #
 # [16-11-2018]
 # * moved LDAP binds to include
-# * added check to verify hosted domains before adding aliases
 
 . ldap_binds.inc
 . msg_formatted.inc
